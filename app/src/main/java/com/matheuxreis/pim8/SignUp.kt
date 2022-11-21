54b6e2961cab2bf3d01e21a7fd9b645e6390e9ea
Matheus Reis <matheus.reis081@gmail.com>
2022-11-21 12:29:56 -0300

    feat: created sign up activity

---
+++ app/src/main/java/com/matheuxreis/pim8/SignUp.kt
@@ -0,0 +1,11 @@
+package com.matheuxreis.pim8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
