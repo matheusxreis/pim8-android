74738000a8f36d69b4b29ff37eb005100b357b29
Matheus Reis <matheus.reis081@gmail.com>
2022-11-21 13:43:16 -0300

    refactor: change way to do screen, using now single activity mode

--- app/src/main/java/com/matheuxreis/pim8/MainActivity.kt
+++ app/src/main/java/com/matheuxreis/pim8/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.matheuxreis.pim8
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(SignIn())
+        bottomNavListener()
     }
-}
+
+
+    fun bottomNavListener(){
+        val authNavBottom = findViewById<NavigationBarView>(R.id.auth_bottom_navigation);
+        authNavBottom.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.auth_page_1 -> {
+                    loadFragment(SignIn())
+                    return@setOnItemSelectedListener true
+
+                }
+                R.id.auth_page_2 -> {
+                    loadFragment(SignUp2())
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    return@setOnItemSelectedListener false
+
+                }
+            }
+        }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+   }
+
+
+
+
--- app/src/main/java/com/matheuxreis/pim8/SignIn.kt
+++ app/src/main/java/com/matheuxreis/pim8/SignIn.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SignUp2.newInstance] factory method to
+ * Use the [SignIn.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SignUp2 : Fragment() {
+class SignIn : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class SignUp2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up2, container, false)
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class SignUp2 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUp2.
+         * @return A new instance of fragment SignIn.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SignUp2().apply {
+            SignIn().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
